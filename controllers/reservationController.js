6705a4d4adc96b10fee5a686b786fa0aea6b1050
T4KOY4KII <[200~191971350+ZigmundSamarista@users.noreply.github.com>
2026-07-16 23:56:51 +0800

    booking crud operation

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -1,3 +1,5 @@
+import Reservation from '../models/Reservation';
+
 const User = require('../models/User');
 const Reservations = require('../models/Reservation');
 const Flight = require('../models/Flight');
@@ -42,3 +44,14 @@ exports.showBooking = (req, res) => {
 };
 
 //Booking logic goes here
+// Create a new booking 
+exports.createBooking = async (req, res) => {
+    const { firstName, lastName, contactCode, contactNumber, 
+        email, passport, nationality, dobMonth, dobDay, dobYear, gender
+    } = req.body;
+
+    // check if all fields are filled out
+    if (!firstName || !lastName ||  !passport || seats == undefined) {
+            return res.status(400).json({ success: false, error: "All fields are required." });
+    }
+}
