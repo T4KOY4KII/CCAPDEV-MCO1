638a502a964766a07056efe646f8a100b0f27b36
T4KOY4KII <[200~191971350+ZigmundSamarista@users.noreply.github.com>
2026-07-17 00:03:33 +0800

    removed previous code

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -45,13 +45,4 @@ exports.showBooking = (req, res) => {
 
 //Booking logic goes here
 // Create a new booking 
-exports.createBooking = async (req, res) => {
-    const { firstName, lastName, contactCode, contactNumber, 
-        email, passport, nationality, dobMonth, dobDay, dobYear, gender
-    } = req.body;
 
-    // check if all fields are filled out
-    if (!firstName || !lastName ||  !passport || seats == undefined) {
-            return res.status(400).json({ success: false, error: "All fields are required." });
-    }
-}
