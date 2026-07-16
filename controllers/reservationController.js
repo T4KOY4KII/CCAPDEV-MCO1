a474b7f646919ecf1e477e4a7aa38f44b6a05e30
T4KOY4KII <[200~191971350+ZigmundSamarista@users.noreply.github.com>
2026-07-17 03:04:38 +0800

    -- added info for booking flight --

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -42,16 +42,44 @@ exports.showBooking = (req, res) => {
 };
 
 //Booking logic goes here
-// Create a new booking 
-exports.createBooking = async (req, res) => {
-    const { firstName, lastName, contactCode, contactNumber, 
-        email, passport, nationality, dobMonth, dobDay, dobYear, gender
-    } = req.body;
 
-    // check if all fields are filled out
-    if (!firstName || !lastName ||  !passport || seats == undefined) {
-            return res.status(400).json({ success: false, error: "All fields are required." });
+
+// Create a new booking 
+async function bookFlight(req, res){
+    try{
+        /* Assuming request body contains necessary details for flight booking */
+
+        const {firstName, lastName, contactCode, contactNumber, email, passport, nationality, dobMonth,
+            dobDay, dobYear, gender,
+        } = req.body;
+
+        // New flight booking document in database //
+        const booking = await User.create({
+            firstName,
+            lastName,
+            contactCode,
+            email,
+            passport,
+            nationality,
+            dobMonth,
+            dobDay,
+            dobYear,
+            gender,
+            type: 'Flight'
+            // Store flight details in database
+        });
+
+        res.status(201).json({booking});
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        });
     }
 }
+
+
+
+
+
 
 
