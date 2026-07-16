74a75b758f6148d32f496b785d445283a781ea3c
Maria Quibranza <maria_teresa_quibranza@dlsu.edu.ph>
2026-07-17 04:59:35 +0800

    removed old reservation func

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -25,15 +25,6 @@ const bookingView = {
     ]
 };
 
-//Renders user reservations page
-exports.showReservations = (req, res) => {
-    res.render('user/reservations', {
-        ...reservationsView
-    });
-};
-
-//User reservations logic goes here
-
 //Renders booking page
 exports.showBooking = (req, res) => {
     res.render('user/booking', {
